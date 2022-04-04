e559b856b5ef9793e5dcc8eb45970e9a07728f22
Mariya Vedernikova <masha239@gmail.com>
2022-04-05 01:01:53 +0300

    extract.py updated

--- src/extract.py
+++ src/extract.py
@@ -1,10 +1,8 @@
 import bs4.element
-import pandas as pd
 from bs4 import BeautifulSoup
 import requests
 from collections import defaultdict
 import numpy as np
-from argparse import ArgumentParser
 import constants
 
 all_percentiles = [60, 70, 80, 90, 100]
@@ -21,7 +19,8 @@ elements = [bs4.element.Comment,
 def process_url(url):
     try:
         data = requests.get(url)
-        if data.status_code == 200:
+
+        if data.status_code == constants.CODE_OK:
             soup = BeautifulSoup(data.text, 'html.parser')
             lens = []
             types = defaultdict(int)
@@ -32,43 +31,15 @@ def process_url(url):
 
             percentiles = [np.percentile(lens, q) for q in all_percentiles]
             el_arr = [float(types[el]) for el in elements]
-            return percentiles + el_arr + [float(len(soup.text))]
+
+            features = percentiles + el_arr + [float(len(soup.text))]
+            return constants.CODE_OK, np.array(features)
         else:
-            return None
-    except BaseException as e:
-        print(e.__class__)
+            return constants.CODE_FAILED, None
 
-
-def prepare_dataframe(infile, outfile, with_labels=False):
-    df_urls = pd.read_csv(infile)
-    features_arr = [process_url(url) for url in df_urls.url]
-
-    percentile_names = [f'p{q}' for q in all_percentiles]
-    element_names = [str(el).split('.')[-1][:-2] for el in elements]
-
-    columns = percentile_names + element_names + ['length']
-    df_features = pd.DataFrame(features_arr, columns=columns)
-
-    if with_labels:
-        df_features[constants.FIELD_IS_TEXT] = df_urls[constants.FIELD_IS_TEXT]
-
-    df_features.to_csv(outfile, index=False)
+    except RuntimeError:
+        return constants.CODE_BAD_REQUEST, None
 
 
 if __name__ == '__main__':
-    parser = ArgumentParser('Prepares features from URL')
-    parser.add_argument('--infile',
-                        type=str,
-                        required=True,
-                        help='Input .csv file with urls')
-    parser.add_argument('--outfile',
-                        type=str,
-                        required=True,
-                        help='Output .csv file with features')
-    parser.add_argument('-l', '--labels',
-                        help='For rewriting labels from infile')
-
-    try:
-        args = parser.parse_args()
-    except SystemExit:
-        parser.print_help()
+    print(process_url('http://yandex.ru'))
