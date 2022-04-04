e32eb9bce1d8f62dfd4d6c1054f851c9d0b88394
Mariya Vedernikova <masha239@gmail.com>
2022-04-05 02:46:24 +0300

    learn.py fixed

--- src/learn.py
+++ src/learn.py
@@ -1,12 +1,15 @@
-import pickle
 import pandas as pd
 import numpy as np
-from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import GridSearchCV, train_test_split
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
 import argparse
 import random
+from skl2onnx import convert_sklearn
+from skl2onnx.common.data_types import FloatTensorType
+from onnxconverter_common import add_metadata_props
 import constants
-
+from datetime import date
 
 def state_seed(seed=0):
     np.random.seed(seed)
@@ -22,8 +25,16 @@ if __name__ == '__main__':
     parser.add_argument('--outfile',
                         type=str,
                         required=True,
-                        help='Output, .onnx file for pretrained classification '
+                        help='Output, .onnx file for pretrained classification'
                              'model')
+    parser.add_argument('--hash',
+                        type=str,
+                        required=True,
+                        help='Commit hash')
+    parser.add_argument('--experiment',
+                        type=str,
+                        required=True,
+                        help='Experiment name')
     parser.add_argument('--seed',
                         type=int,
                         default=0,
@@ -35,12 +46,29 @@ if __name__ == '__main__':
     X = df.drop(constants.FIELD_IS_TEXT, axis=1).values
     y = df[constants.FIELD_IS_TEXT].values
 
+    X_train, X_val, y_train, y_val = train_test_split(X,
+                                                      y,
+                                                      test_size=0.2,
+                                                      random_state=args.seed)
+
     parameters = {'max_depth': [3, 4, 5], 'min_samples_split': range(2, 10)}
     rf = RandomForestClassifier(random_state=args.seed)
-    clf = GridSearchCV(rf, parameters)
-    clf.fit(X, y)
+    clf = GridSearchCV(rf, parameters, cv=3)
+    clf.fit(X_train, y_train)
 
-    print(clf.best_score_, clf.best_params_)
+    estimator = clf.best_estimator_
+    print(f'Best score on CV {clf.best_score_:.3},'
+          f'best params {clf.best_params_}')
+    print(f'Accuracy on validation set '
+          f'{accuracy_score(estimator.predict(X_val), y_val):.3}')
 
-    with open(args.outfile, 'wb') as file:
-        pickle.dump(clf.best_estimator_, file)
+    initial_type = [('float_input', FloatTensorType([None, X.shape[1]]))]
+    onx = convert_sklearn(estimator, initial_types=initial_type)
+
+    params = {'hash_commit': args.hash,
+              'created': date.today().__str__(),
+              'experement': args.experiment}
+    add_metadata_props(onx, params, 15)
+
+    with open(args.outfile, 'wb') as f:
+        f.write(onx.SerializeToString())
